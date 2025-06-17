13dd230c3a1d4a3598acd03e66086bccf92257a5
Santhosh Jenova | Gen AI & Data Science Engineer <santhoshjenova@gmail.com>
2025-06-17 16:54:54 +0530

    Add files via upload

---
+++ 30 day python Day 1 Print .py
@@ -0,0 +1,2 @@
+#Day1
+print("Hello World")
---
+++ 30 day python Day 2.Variables and Data.py
@@ -0,0 +1,8 @@
+#Day2
+# Get user input for length and width
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+# Calculate the area
+area = length * width
+# Print the result
+print("The area of the rectangle is:", area)
